1c10bbf190a062ffcd83b4d00811af9f64557fbb
Enock Chikobvu <sechikobvu@gmail.com>
2021-12-20 21:16:57 +0200

    finished all pages

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { MdCancel } from 'react-icons/md';
+import { Box, Flex, Select } from '@chakra-ui/react';
+import { filterData, getFilterValues } from '../utils/filterData';
+
+function searchFilters() {
+  const router = useRouter();
+  const [filters, setFilters] = useState(filterData);
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+      if (item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value;
+      }
+    });
+
+    router.push({ pathname: path, query });
+  };
+
+  return (
+    <Flex flexWrap="wrap" bg="gray.100" p="4" justifyContent="center">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter.items.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
+}
+
+export default searchFilters;
